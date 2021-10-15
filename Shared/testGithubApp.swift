574b8c3244b42695fe0127cc1161b8dc9de6d271
RosalieWessels <rosalie.wessels@yahoo.com>
2021-10-15 11:48:13 -0700

    Initial Commit

---
+++ Shared/testGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGithubApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct testGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
